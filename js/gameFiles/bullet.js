41190ee2cccad05c37a76d4d979c09ce772f0631
TarshenNaidoo <58662650+TarshenNaidoo@users.noreply.github.com>
2020-05-20 02:23:23 +0200

    Updated bullet
    I modified the shoot function so it now works. Added animate function so the bullet travels

--- js/gameFiles/bullet.js
+++ js/gameFiles/bullet.js
@@ -1,19 +1,15 @@
 class Bullet {
 
-    constructor(i) {
+    constructor() {
 
         this.material = new THREE.MeshStandardMaterial(0xeeeeee);
         this.bulletWidth = 1;
         this.launched = false;
-        this.bullet =
-            new THREE.Mesh(
-                new THREE.SphereBufferGeometry(this.bulletWidth/4, 20, 20), this.material, 50);
-        this.bullet.castShadow = true;
-        this.bullet._dirtyPosition = true;
-        this.bullet.position.y = 5;
-        this.bullet.position.x = -10 + i;
-        this.target = new THREE.Vector3 (0,0,0);
-        //this.bullet.visible = false;
+        this.bullet = bullet;
+        bullet.position.y = height;
+        this.direction = new THREE.Vector3 (0,0,0);
+        this.initialPosition = new THREE.Vector3(0,0,0);
+        this.speed = 0;
     }
 
     getLaunched() {
@@ -29,25 +25,44 @@ class Bullet {
             z: this.bullet.position.z, radius: this.bulletWidth/2};
     }
 
-    shoot(position, target, power) {
-        this.bullet.visible = true;
-        this.target.set(target.x, target.y, target.z);
-        this.bullet.position.set(position.x-target.x, position.y+5, position.z-target.z);
+    animate() {
 
-        this.bullet.setCcdMotionThreshold(10);
-        this.bullet.setCcdSweptSphereRadius(this.bulletWidth/4);
+        if (this.launched) {
+            this.bullet.position.x += this.direction.x*delta * this.speed;
+            this.bullet.position.y += this.direction.y*delta * this.speed;
+            this.bullet.position.z += this.direction.z*delta * this.speed;
 
-        this.bullet.__dirtyPosition = true;
+            if (
+                Math.sqrt(
+                    Math.pow(
+                        this.bullet.position.x-this.initialPosition.x,
+                        2
+                    ) +
+                    Math.pow(
+                        this.bullet.position.y - this.initialPosition.y,
+                        2
+                    ) +
+                    Math.pow(this.bullet.position.z - this.initialPosition.z,
+                        2
+                    )
+                ) > 50
+            ) {this.bullet.visible = false; this.launched = false}
+        }
+    }
+
+    shoot(position, speed) {
         this.launched = true;
+        this.bullet.visible = true;
+        this.speed = speed;
+        this.direction.set(camera.getWorldDirection().x, camera.getWorldDirection().y, camera.getWorldDirection().z);
+        this.direction.normalize();
+        this.bullet.rotation.set(camera.rotation.x, camera.rotation.y-Math.PI/2, camera.rotation.z);
+        this.bullet.position.set(camera.position.x + 3*this.direction.x, camera.position.y + 3*this.direction.y, camera.position.z + 3*this.direction.z);
+
+        this.initialPosition.set(camera.position.x, camera.position.y, camera.position.z);
 
         //let sound = null;
-        let force = new THREE.Vector3(
-            this.target.x*power,
-            this.target.y*power,
-            this.target.z*power);
 
         //sound.play;
-
-        this.bullet.applyCentralImpulse(force);
     }
 }
