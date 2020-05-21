d00c6ccc043190d66f71822d4512282a36ffbada
TarshenNaidoo <58662650+TarshenNaidoo@users.noreply.github.com>
2020-05-22 00:37:42 +0200

    Added skybox file
    Added skybox file. Used another version since the original had cars and houses and is not thematic to the game

---
+++ src/js/gameFiles/skybox.js
@@ -0,0 +1,32 @@
+class Skybox{
+
+    constructor() {
+        this.materialArray = [];
+
+        this.texture_ft = new THREE.TextureLoader().load('../../src/textures/skybox2/sh_ft.png');
+        this.texture_bk = new THREE.TextureLoader().load('../../src/textures/skybox2/sh_bk.png');
+        this.texture_up = new THREE.TextureLoader().load('../../src/textures/skybox2/sh_up.png');
+        this.texture_dn = new THREE.TextureLoader().load('../../src/textures/skybox2/sh_dn.png');
+        this.texture_rt = new THREE.TextureLoader().load('../../src/textures/skybox2/sh_rt.png');
+        this.texture_lf = new THREE.TextureLoader().load('../../src/textures/skybox2/sh_lf.png');
+
+        this.materialArray.push(new THREE.MeshBasicMaterial({map: this.texture_ft}));
+        this.materialArray.push(new THREE.MeshBasicMaterial({map: this.texture_bk}));
+        this.materialArray.push(new THREE.MeshBasicMaterial({map: this.texture_up}));
+        this.materialArray.push(new THREE.MeshBasicMaterial({map: this.texture_dn}));
+        this.materialArray.push(new THREE.MeshBasicMaterial({map: this.texture_rt}));
+        this.materialArray.push(new THREE.MeshBasicMaterial({map: this.texture_lf}));
+
+        for (let i = 0 ; i < this.materialArray.length ; i++) {
+            this.materialArray[i].side = THREE.BackSide;
+            this.materialArray[i].frustumCulled = false;
+        }
+
+        this.skyboxGeo = new THREE.BoxGeometry(2000,2000,2000);
+        this.skybox = new THREE.Mesh(this.skyboxGeo, this.materialArray);
+    }
+
+    getSkybox(){
+        return this.skybox;
+    }
+}
