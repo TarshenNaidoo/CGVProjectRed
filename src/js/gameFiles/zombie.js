bf9de669cd004ec363b210627de71ce6c9b32083
Hollow-Spectre <61150287+Hollow-Spectre@users.noreply.github.com>
2020-05-22 13:09:52 +0200

    Update zombie.js
    added zombie movement

--- src/js/gameFiles/zombie.js
+++ src/js/gameFiles/zombie.js
@@ -97,6 +97,7 @@ class Zombie {
                     //checks if Zombie is close enough then stops moving
                     let radius = Math.sqrt(Math.pow(zDirection.x,2) + Math.pow(zDirection.z,2));
                     if (radius < 5){
+			    avatar.hp--;
                         return;
                     }
                     //
